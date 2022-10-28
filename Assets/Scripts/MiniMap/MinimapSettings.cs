e74e27f3f9d37a19dba14ed9ca1a0473c1480166
Dan nguyen <nguyensydan25@gmail.com>
2022-10-28 18:38:27 +0700

    Revert "no message"
    This reverts commit 855d299e25e90206fa4945be6eca29b697611ec7.

--- Assets/Scripts/MiniMap/MinimapSettings.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class MinimapSettings : MonoBehaviour
-{
-    public Transform targetToFollow;
-    public bool rotateWithTheTarget = true;
-}
